b9a5e23cad6fdb3547642c773c2f7cdebbf88f9e
Janhavi Tak <115899701+janhavitak18@users.noreply.github.com>
2025-01-29 10:45:07 +0530

    Time: 27 ms (48.33%), Space: 85.9 MB (30.32%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,23 +1,49 @@
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
 
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<int> s;
-        heights.push_back(0);
-        int maxArea = 0;
-        int index = 0;
+        int n = heights.size();
+        vector<int> left = getLeft(heights, n);
+        vector<int> right = getRight(heights, n);
+        int res = 0;
 
-        while (index < heights.size()) {
-            if (s.empty() || heights[index] >= heights[s.top()]) {
-                s.push(index++);
-            } else {
-                int height = heights[s.top()];
-                s.pop();
-                int width = s.empty() ? index : index - s.top() - 1;
-                maxArea = max(maxArea, height * width);
-            }
+        for(int i = 0; i < n; i++) {
+            res = max(res, heights[i] * (right[i] - left[i] - 1));
         }
+        return res;
+    }
 
-        return maxArea;
+private:
+    vector<int> getLeft(vector<int>& heights, int n) {
+        stack<int> st;
+        vector<int> left(n);
+
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            left[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        return left;
+    }
+
+    vector<int> getRight(vector<int>& heights, int n) {
+        stack<int> st;
+        vector<int> right(n);
+
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            right[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        return right;
     }
 };
