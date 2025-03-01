600d40f00e08f42f6c92313c5c00ae8b506c5e8c
Janhavi Tak <115899701+janhavitak18@users.noreply.github.com>
2025-03-01 17:37:51 +0530

    Time: 0 ms (100%), Space: 13.1 MB (87.59%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] = nums[i] * 2;
+                nums[i + 1] = 0;
+            }
+        }
+        int l = 0;
+            for (int r = 0; r < n; r++) {
+                if (nums[r] != 0) {
+                    swap(nums[r], nums[l]);
+                    l++;
+                }
+            }
+        // reverse(nums.begin(),nums.end());
+        return nums;
+    }
+};
+
+
+//[1 4 0 2 0 0]
