6fcd8eec1ec16b56849f764cad400fd872e4c2bf
Janhavi Tak <115899701+janhavitak18@users.noreply.github.com>
2025-06-23 11:20:12 +0530

    Time: 3 ms (75.19%), Space: 28.6 MB (22.25%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -2,25 +2,48 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& a) {
         int n = a.size();
-        vector<int>v(n,-1);
-
-        stack<int>st;
-        for(int i = 2*n - 1; i >= 0; i--)
-        {
-            
-            while(!st.empty() && (a[i%n] >= st.top()))
-            {
+        vector<int> result(n, -1);
+        stack<int> st; 
+        for (int i =2* n - 1; i >= 0; i--) {
+            int originalidx;
+            if (i < n) {
+                originalidx = i;
+            } else {
+                originalidx = i - n;
+            }
+            while (!st.empty() && a[st.top()] <= a[originalidx]) {
                 st.pop();
             }
 
-            if(!st.empty() && (i < n))
-            {
-                v[i] = st.top();
+            if (i < n && !st.empty()) {
+                result[originalidx] = a[st.top()];
             }
 
-            st.push(a[i%n]);
+            st.push(originalidx);
         }
 
-        return v;
+        return result;
+
+        // int n = a.size();
+        // vector<int>v(n,-1);
+
+        // stack<int>st;
+        // for(int i = 2*n - 1; i >= 0; i--)
+        // {
+            
+        //     while(!st.empty() && (a[i%n] >= st.top()))
+        //     {
+        //         st.pop();
+        //     }
+
+        //     if(!st.empty() && (i < n))
+        //     {
+        //         v[i] = st.top();
+        //     }
+
+        //     st.push(a[i%n]);
+        // }
+
+        // return v;
     }
 };
